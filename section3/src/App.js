c97e20b2ab0067f3f9e46221f887398a60da8183
unknown <d>
2021-10-31 21:10:49 -0600

    Initial commit of code

---
+++ section3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
