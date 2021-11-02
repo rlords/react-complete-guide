d0d636fd9a5c47490431a508588ae08fadfd2e0a
unknown <d>
2021-11-01 21:52:30 -0600

    Initial commit of section 4

---
+++ section4/src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+
+import './Expenses.css'
+
+function Expenses(props) {
+    return (
+        <Card className="expenses">
+            {props.expenses.map(ex => {
+               return <ExpenseItem title={ex.title} amount={ex.amount} date={ex.date}/>
+            })};
+        </Card>
+    )
+}
+
+export default Expenses
