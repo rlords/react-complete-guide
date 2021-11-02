18fe1d94e82d13efcbd0eef6669151220e9ccdd0
unknown <d>
2021-11-01 20:28:25 -0600

    Added Card and updated expenses

--- section3/src/components/Expenses.js
+++ section3/src/components/Expenses.js
@@ -1,14 +1,15 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 import './Expenses.css'
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {props.expenses.map(ex => {
                return <ExpenseItem title={ex.title} amount={ex.amount} date={ex.date}/>
             })};
-        </div>
+        </Card>
     )
 }
 
