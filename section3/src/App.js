db2c40ef97a302afa0b3ad483437bfa49051961b
unknown <d>
2021-11-01 19:23:37 -0600

    Added ExpenseDate, updated ExpenseItem

--- section3/src/App.js
+++ section3/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
-      <ExpenseItem />
+      <ExpenseItem date={new Date()} title="Expense Item 1" amount="123.56"/>
     </div>
   );
 }
