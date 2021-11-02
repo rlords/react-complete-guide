244bff19f4cf9e0b07c65ce80bdc93939e3e5ced
unknown <d>
2021-11-01 22:41:23 -0600

    Expense item first test with even handlers

--- section4/src/components/Expenses/ExpenseItem.js
+++ section4/src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,13 @@ import Card from '../UI/Card';
 
 import './ExpenseItem.css'
 
-function ExpenseItem (props) {
+const ExpenseItem = (props) => {
+
+
+    const clickHandler = () => {
+        console.log('Clicked!')
+    }
+
     return(
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
@@ -11,7 +17,7 @@ function ExpenseItem (props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
