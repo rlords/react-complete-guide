34f7ba184ac8e66b53cd778632841f9351971253
unknown <d>
2021-11-02 21:52:01 -0600

    Updated expense items, added new expenses.

--- section4/src/App.js
+++ section4/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -26,8 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
